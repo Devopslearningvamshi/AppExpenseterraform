829379327f431a7663f60db69a510f80574758bd
PATEL VAMSHI DHAR REDDY <49507145+patelvamshidhar@users.noreply.github.com>
2024-05-08 01:15:43 +0530

    updes

--- pipelines/modules/apps/main.tf
+++ pipelines/modules/apps/main.tf
@@ -8,6 +8,7 @@ tags = {
 }
 }
 resource "null_resource" "ansible"{
+
     provisioner "remote-exec" {
 
     connection {
@@ -16,14 +17,16 @@ resource "null_resource" "ansible"{
       password = var.ssh_pass
       host     = aws_instance.instance.public_ip
     }
+
     inline = [
         "sudo pip3.11 install ansible",
-        "ansible-playbook -i localhost , -U https://github.com/Devopslearningvamshi/gocd-expenseapp expense.yml -e env=${var.env} -e role_name= ${var.compenent"
+        "ansible-playbook -i localhost , -U https://github.com/Devopslearningvamshi/gocd-expenseapp expense.yml -e env=${var.env} -e role_name= ${var.compenent}"
     ]
+    
+    }
+}
 
-}
-}
-resource "aws_route53_record" "record"{
+resource "aws_route53_record" "record" {
     name = "${var.compenent}-${var.env}"
     type = "A"
     zone_id = var.zone_id
