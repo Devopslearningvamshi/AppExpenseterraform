5b80e2841b013bce075ed206c91431f0e305bf72
PATEL VAMSHI DHAR REDDY <49507145+patelvamshidhar@users.noreply.github.com>
2024-05-09 23:11:33 +0530

    new code

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "frontend"{
-    source = "./Devopslearningvamshi/AppExpenseterraform/pipelines/modules/apps"
+    source = "./modules/apps"
     instance_type = var.instance_type
     compenent = "frontend"
     ssh_user = var.ssh_user
     ssh_pass = var.ssh_pass
-    env = var.env
-    zone_id= var.zone_id
+    env      = var.env
+    zone_id  = var.zone_id
 }
 
 #
