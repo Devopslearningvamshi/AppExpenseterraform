db5e85ad46170351419ba049becef60b88f61504
PATEL VAMSHI DHAR REDDY <49507145+patelvamshidhar@users.noreply.github.com>
2024-05-10 00:23:40 +0530

    new udpate added

--- variable.tf
+++ variable.tf
@@ -1,6 +1,2 @@
-variable "instance_type"{}
-variable "component"{}
-variable "ssh_user"{}
-variable "ssh_pass" {}
-variable "zone_id"{}
-variable "env"{}
+variable "env" {}
+variable "instance_type" {}
