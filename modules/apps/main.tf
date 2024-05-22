255edf07f9cb0352ef8c3512eb8d611c543ca86d
PATEL VAMSHI DHAR REDDY <49507145+patelvamshidhar@users.noreply.github.com>
2024-05-22 15:20:02 +0530

    change done

--- modules/apps/main.tf
+++ modules/apps/main.tf
@@ -8,21 +8,21 @@ tags = {
     }
 }
 resource "null_resource" "ansible" {
-    provisioner "remote-exec" {
+  provisioner "remote-exec" {
 
-     connection {
-         type     = "ssh"
-        user     = var.ssh_user
-        password = var.ssh_pass
-         host     = aws_instance.instance.public_ip
+    connection {
+      type     = "ssh"
+      user     = var.ssh_user
+      password = var.ssh_pass
+      host     = aws_instance.instance.public_ip
     }
 
     inline = [
         "sudo pip3.11 install ansible",
         "ansible-playbook -i localhost , -U https://github.com/Devopslearningvamshi/gocd-expenseapp expense.yml -e env=${var.env} -e role_name= ${var.component}"
     ]
-    
-    }
+  }
+
 }
 
 resource "aws_route53_record" "record" {
