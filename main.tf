5f0f53206aa719093940a1ecd69b0957ed09563f
patelvamshidhar <reddyvamshi607@gmail.com>
2024-05-26 12:22:36 +0530

    Dependese add in the backend modules

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ module "frontend" {
     zone_id  = var.zone_id
 }
 module "backend" {
+    depends_on = [module.mysql]
     source = "./modules/apps"
     instance_type = var.instance_type
     component = "backend"
