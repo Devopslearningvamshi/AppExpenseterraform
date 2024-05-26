55338c576df2009199a75491e3f4b185fb48053b
patelvamshidhar <reddyvamshi607@gmail.com>
2024-05-26 11:45:41 +0530

    Backend,mysql added in the modules

--- main.tf
+++ main.tf
@@ -7,12 +7,21 @@ module "frontend" {
     env      = var.env
     zone_id  = var.zone_id
 }
-
-#
-# module "backendtf"{
-#     source = "./modules/app"
-# }
-
-# module "mysqltf"{
-#     source = "./modules/app"
-# }
+module "backend" {
+    source = "./modules/apps"
+    instance_type = var.instance_type
+    component = "backend"
+    ssh_user = var.ssh_user
+    ssh_pass = var.ssh_pass
+    env      = var.env
+    zone_id  = var.zone_id
+}
+module "mysql" {
+    source = "./modules/apps"
+    instance_type = var.instance_type
+    component = "mysql"
+    ssh_user = var.ssh_user
+    ssh_pass = var.ssh_pass
+    env      = var.env
+    zone_id  = var.zone_id
+}
